5a9311c9511a54bb1cc109303daf27d257187a2f
@rodiontazetdinov <rodiontazetdinov@gmail.com>
2022-12-02 20:29:09 +0300

    feat: добавить sass, добавить компонент Project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,19 @@
 // import logo from './logo.svg';
+// import '../../index.sass'
 import './App.sass';
 
+
 import Header from '../Header/Header';
+import Project from '../Project/Project';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Project/>
+      <Project/>
+      <Project/>
+
     </div>
   );
 }
