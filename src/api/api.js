cde38f6369525c4dcc8f55026ddf95dbbd5461e2
@rodiontazetdinov <rodiontazetdinov@gmail.com>
2022-12-08 20:33:46 +0300

    feat: добавить api для взаимодействия с localstorage

--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,16 @@
-// создаём объект
-// const user = { name: 'Ivan', age: 23 };
-// // преобразовываем объект в строку и сохраняем в localStorage
-// localStorage.setItem('user', JSON.stringify(user));
-// // извлекаем данные и преобразовываем в объект
-// const savedUser = JSON.parse(localStorage.getItem('user'));
-
-// localStorage.setItem('storedUsers', JSON.stringify(users));
-// ...
-// var users = [];
-// if (localStorage.getItem('storedUsers'))
-//   users = JSON.parse(localStorage.getItem('storedUsers'));
-
 export const getProjects = () => {
   if (localStorage.getItem('projects')) {
+    console.log(localStorage.getItem('projects'));
     const projects = JSON.parse(localStorage.getItem('projects'));
+    console.log(projects)
     return projects;
   }
-  return '';
+  const project = [{id: '1', title: 'Ваш первый проект', todosCount: 0}];
+  localStorage.setItem('projects', JSON.stringify(project));
+  // нужно ли будет возвращать?
+  return project;
+  //или пустая страница
+  // return '';
 
 };
 
@@ -24,9 +18,11 @@ export const addProject = (id, title, todosCount) => {
   if (localStorage.getItem('projects')) {
     const projects = JSON.parse(localStorage.getItem('projects'));
     projects.push({id, title, todosCount});
+    localStorage.setItem('projects', JSON.stringify(projects));
     // нужно ли будет возвращать?
     return projects;
   }
+
   const project = [{id, title, todosCount}];
   localStorage.setItem('projects', JSON.stringify(project));
   // нужно ли будет возвращать?
