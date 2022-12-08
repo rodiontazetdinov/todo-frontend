65ac4e83330dc5349fcf2f121b64e54733b6478c
@rodiontazetdinov <rodiontazetdinov@gmail.com>
2022-12-08 16:26:52 +0300

    feat: добавить начальную инициализацию проектов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,21 @@
 // import '../../index.sass'
 import './App.sass';
 import Main from '../Main/Main';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { loadProjects } from '../../actions';
+import * as api from '../../api/api.js';
 
 
 import Header from '../Header/Header';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadProjects(api.getProjects()));
+  }, []);
+
   return (
     <div className="App">
       <Header/>
