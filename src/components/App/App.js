9a781bf39eda9109f2dc9b9e406fe38d991d5e0e
@rodiontazetdinov <rodiontazetdinov@gmail.com>
2022-12-09 15:14:15 +0300

    refactor: незначительные изменения

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,15 @@
-// import logo from './logo.svg';
-// import '../../index.sass'
 import './App.sass';
 import Main from '../Main/Main';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { loadProjects } from '../../actions';
-import * as api from '../../api/api.js';
-
 
 import Header from '../Header/Header';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadProjects(api.getProjects()));
-  }, []);
 
   return (
     <div className="App">
       <Header/>
       <Main>
       </Main>
-
-
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,10 +9,6 @@ import { Route, Routes } from 'react-router-dom';
 function Main() {
   return (
     <div className='main'>
-      {/* временные прожекты для визуализации наполнения */}
-      <Project></Project>
-      <Project></Project>
-      <Project></Project>
       <Routes>
         <Route
           path={'/tasks'}
