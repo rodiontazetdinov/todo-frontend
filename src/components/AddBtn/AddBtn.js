3a8d6b0e23a8ec1a433002616690a4d51edbda16
@rodiontazetdinov <rodiontazetdinov@gmail.com>
2022-12-14 16:25:05 +0300

    feat: Добавить кнопку addProject

---
+++ src/components/AddBtn/AddBtn.js
@@ -0,0 +1,22 @@
+import './AddBtn.sass'
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { toggleAddProjectPopupOpened } from '../../actions/index'
+
+function AddBtn() {
+  const dispatch = useDispatch();
+
+  const openProjectAddPopup = () => {
+    dispatch(toggleAddProjectPopupOpened(true));
+    console.log('add_popup_opened')
+  };
+
+  return (
+    <li className='add-btn'>
+      <input className='add-btn__btn' type='button' onClick={openProjectAddPopup}/>
+    </li>
+  );
+}
+
+export default AddBtn;
