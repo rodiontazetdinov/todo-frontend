ef7b79ac02b2ddcc38b4c61e0b95c9d766aaa8e6
@rodiontazetdinov <rodiontazetdinov@gmail.com>
2022-12-14 16:08:35 +0300

    feat: добавить экшн открытия попапа в reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,7 @@
 const initialState = {
   projects: [],
-  projectsLoaded: false
+  projectsLoaded: false,
+  isAddProjectPopupOpened: false
 }
 
 const reducer = (state = initialState, action) => {
@@ -14,6 +15,11 @@ const reducer = (state = initialState, action) => {
         return {
             ...state,
             projectsLoaded: true
+        }
+      case 'TOGGLE_ADD_PROJECT_POPUP_OPENED':
+        return {
+            ...state,
+            isAddProjectPopupOpened: action.payload
         }
       default: return state
   }
